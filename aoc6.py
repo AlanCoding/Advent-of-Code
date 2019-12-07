d6b88aa1cdae6546e0a4eea49438136aef150fcc
AlanCoding <arominge@redhat.com>
2019-12-06 22:23:00 -0500

    Day 6 part 2

--- aoc6.py
+++ aoc6.py
@@ -38,18 +38,8 @@ def solve_problem(input):
         stack.reverse()
         for thing in stack:
             orbit_counts[thing] = orbit_counts[parents[thing]] + 1
-        # current_ct = orbit_counts[about]
-        # the_ct = current_ct + 1
-        # orbit_counts[object] = the_ct
 
     return sum(orbit_counts.values())
-
-        # if object in orbit_counts:
-        #     raise Exception(f'{object} orbits multiple things, line {i}')
-        # elif about not in orbit_counts:
-        #     raise Exception(f'Object {object} orbits unknown thing {about}, line {i}')
-        #     # orbit_counts[object] = 0
-        #     continue
 
 
 # part 1
@@ -67,3 +57,67 @@ for scenario in scenarios:
 
 # 932, too low
 
+print('')
+print('**** PART 2 ******')
+
+
+def get_parents(key, parents):
+    """Get parents for a particular key through parents dict"""
+    r = [key]
+    while True:
+        this_one = r[-1]
+        if this_one == 'COM':
+            return r
+        r.append(parents[this_one])
+
+
+def part2(input):
+    parents = {}
+    for i, line in enumerate(input.split('\n')):
+        about, object = line.split(')')
+        parents[object] = about
+
+    santa = get_parents('SAN', parents)
+    me = get_parents('YOU', parents)
+
+    for i, planet in enumerate(me):
+        if planet in santa:
+            print(f'met at {planet}')
+            print('')
+            print(santa[:santa.index(planet) + 1])
+            print(len(santa[:santa.index(planet) + 1]))
+            # minus one because we want traversials between elements in list
+            print(santa.index(planet))
+            print('')
+            print(me[:i + 1])
+            print(len(me[:i + 1]))
+            # minus one because we want traversials between elements in list
+            print(i)
+            # minus another one because transfering to the planet is already counted
+            # ...or something like that
+            # minus one because problem said so
+            return i + santa.index(planet) - 1
+
+data['example'] = """COM)B
+B)C
+C)D
+D)E
+E)F
+B)G
+G)H
+D)I
+E)J
+J)K
+K)L
+K)YOU
+I)SAN"""
+
+for scenario in scenarios:
+    input = data[scenario]
+    r = part2(input)
+    print(f'Part 2 answer {r}')
+
+# 432, too high
+# 433, too high
+# 431, too high
+# 430, correct
