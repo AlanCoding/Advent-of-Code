39ec18273c8db48e8db2bece1766c0d59ffab5f7
AlanCoding <arominge@redhat.com>
2019-12-10 12:13:24 -0500

    this works too

--- aoc10.py
+++ aoc10.py
@@ -1,3 +1,6 @@
+from itertools import chain
+
+
 with open(__file__.replace('.py', '.txt')) as f:
     input = f.read()
 
@@ -63,23 +66,41 @@ data = {
 
 
 def can_see(p1, p2, asteroids):
-    if p1 == p2:
-        return False  # do not count self
-    if p1[0] == p2[0]:
-        y_sign = abs(p2[1] - p1[1]) // (p2[1] - p1[1])
-        for y in range(p1[1] + y_sign, p2[1], y_sign):
-            if (p1[0], y) in asteroids:
+    delta_p = tuple(p2[j] - p1[j] for j in range(2))
+    for (j, k) in ((1, 0), (0, 1)):
+        if delta_p[j] == 0:
+            if delta_p[k] == 0:
+                return False  # do not count self
+            continue
+        sign = delta_p[j] // abs(delta_p[j])
+        # loop over all candidate fractions
+        for delta_j in range(sign, delta_p[j], sign):
+            if abs(delta_p[k] * delta_j) % abs(delta_p[j]) != 0:
+                continue  # fraction did not divide other dimension, not in center
+            delta_k = (delta_p[k] * delta_j) // delta_p[j]  # no remainder
+            this_point = [None for i in range(2)]
+            this_point[j] = p1[j] + delta_j
+            this_point[k] = p1[k] + delta_k
+            if tuple(this_point) in asteroids:
                 return False
-    else:
-        # loop through all (x, y) points on straight line
-        x_sign = abs(p2[0] - p1[0]) // (p2[0] - p1[0])
-        for x in range(p1[0] + x_sign, p2[0], x_sign):
-            if abs((p2[1] - p1[1]) * (x - p1[0])) % abs(p2[0] - p1[0]) != 0:
-                continue  # did not pass through center of box
-            delta_y = ((p2[1] - p1[1]) * (x - p1[0])) // (p2[0] - p1[0])
-            y = p1[1] + delta_y
-            if (x, y) in asteroids:
-                return False
+
+    # if delta_p[0] == 0:
+    #     if delta_p[1] == 0:
+    #         return False  # do not count self
+    #     y_sign = abs(delta_p[1]) // delta_p[1]
+    #     for y in range(p1[1] + y_sign, p2[1], y_sign):
+    #         if (p1[0], y) in asteroids:
+    #             return False
+    # else:
+    #     # loop through all (x, y) points on straight line
+    #     x_sign = abs(delta_p[0]) // delta_p[0]
+    #     for x in range(p1[0] + x_sign, p2[0], x_sign):
+    #         if abs(delta_p[1] * (x - p1[0])) % abs(delta_p[0]) != 0:
+    #             continue  # did not pass through center of box
+    #         delta_y = ((delta_p[1]) * (x - p1[0])) // (delta_p[0])
+    #         y = p1[1] + delta_y
+    #         if (x, y) in asteroids:
+    #             return False
     return True
 
 
