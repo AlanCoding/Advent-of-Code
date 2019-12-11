634c0e4e716b472b6cb21f43fc10a1bd17be626a
AlanCoding <arominge@redhat.com>
2019-12-10 19:54:35 -0500

    Day 10 part 2

--- aoc10.py
+++ aoc10.py
@@ -1,4 +1,6 @@
 from fractions import Fraction
+from math import atan2, pi
+import json
 
 
 with open(__file__.replace('.py', '.txt')) as f:
@@ -91,15 +93,18 @@ def visible_directions(p, asteroids):
     return directions
 
 
-def solve_problem(input):
+def asteroids_from_input(input):
     asteroids = set()
     for y, line in enumerate(input.split('\n')):
         for x, character in enumerate(line):
             if character == '#':
                 asteroids.add((x, y))
 
-    asteroids = frozenset(asteroids)  # lock down modifications
-    print('  total asteroid count: {0}'.format(len(asteroids)))
+    return frozenset(asteroids)  # lock down modifications
+
+
+def solve_problem(input):
+    asteroids = asteroids_from_input(input)
 
     max_see = 0
     for this_loc in asteroids:
@@ -124,7 +129,74 @@ print('')
 print('***** PART 2 ******')
 
 
-# for name, input in data.items():
-#     print('')
-#     see_ct, location = solve_problem(input)
+def angle_from_vertial(p):
+    # the "up" in this case is (0, -1), we want to count up from there
+    # going in the clockwise direction
+    # ratio = -1 * p[0] * p[1]
+    return (atan2(p[1], p[0]) + pi / 2.) % (2 * pi)
+
+
+print('')
+print('testing angle function')
+print('These should all increase in order for sorting to be right')
+print('dead up {}'.format(angle_from_vertial((0, -1))))
+print('Upper right {}'.format(angle_from_vertial((1, -1))))
+print('dead right {}'.format(angle_from_vertial((1, 0))))
+print('lower right {}'.format(angle_from_vertial((1, 1))))
+print('lower left {}'.format(angle_from_vertial((-1, 1))))
+print('Upper left {}'.format(angle_from_vertial((-1, -1))))
+
+
+
+def mag(p):
+    return (p[0]**2 + p[1]**2)**(0.5)
+
+
+def part2(loc, input, shots=200):
+    asteroids = asteroids_from_input(input)
+    asteroids_by_direction = {}
+    for pt in asteroids:
+        direction = get_direction_vector(loc, pt)
+        if direction in asteroids_by_direction:
+            asteroids_by_direction[direction].append(pt)
+        else:
+            asteroids_by_direction[direction] = [pt]
+
+    for dir in list(asteroids_by_direction.keys()):
+        asteroids_by_direction[dir] = sorted(asteroids_by_direction[dir], key=mag)
+
+    directions_and_asteroids = [(dir, roids) for dir, roids in asteroids_by_direction.items()]
+
+    directions_and_asteroids = sorted(directions_and_asteroids, key=lambda entry: angle_from_vertial(entry[0]))
+
+    Ndirs = len(directions_and_asteroids)
+    pos = 0
+    for shot in range(shots):
+        targets = []
+        local_ct = 0
+        while not targets:
+            targets = directions_and_asteroids[pos][1]
+            pos += 1
+            if pos >= Ndirs:
+                pos = 0
+            local_ct += 1
+            if local_ct > Ndirs * 2:
+                print('\n'.join([str(t) for t in directions_and_asteroids]))
+                print(f'Position {pos}, shot {shot}')
+                raise RuntimeError('Iteration problem')
+        pt = targets.pop()
+
+    return pt
+
+
+for name, input in data.items():
+    print('')
+    see_ct, location = solve_problem(input)
+    if name in ('problem', '5'):
+        shots = 201
+    else:
+        shots = input.count('#') // 2
+    r = part2(location, input, shots=shots)
+    print(f'Answer for {name}: {shots}th asteroid is {r}')
+    print('  multiply its X coordinate by 100 and then add its Y coordinate {}'.format(r[0] * 100 + r[1]))
 
